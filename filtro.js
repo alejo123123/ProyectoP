e98fc7ec8c03905effd342c8dc92bf1f493473d7
Alejo <alejobustamanter@gmail.com>
2024-06-26 18:14:32 -0500

    agregue una funciona para cuando le den click al boton de mostrar todo

--- filtro.js
+++ filtro.js
@@ -1,10 +1,11 @@
 const botmaquillaje = document.querySelector('#maqui')
 const maquillaje = document.querySelectorAll('#maquillaje')
-const unas = document.querySelector('#unas')
+const unas = document.querySelectorAll('#unas')
 const botuna = document.querySelector('#unabot')
 const botfiltro = document.querySelector('#filtro')
 const labialp = document.querySelectorAll('#labial')
 const botlabial = document.querySelector('#labot')
+const botodo = document.querySelector('#todo')
 
     
     if(botuna) {
@@ -12,18 +13,22 @@ const botlabial = document.querySelector('#labot')
         botuna.addEventListener('click', function() {
 
     if (maquillaje.length > 0) {
-        unas.classList.remove('hidden')
+
+        unas.forEach(item => {
+            item.classList.remove('hidden')
+        })
 
         maquillaje.forEach(item => {
             item.classList.add('hidden')
         })
+
         labialp.forEach(item => {
             item.classList.add('hidden')
         
     })
 
     } else {
-    console.log('sexp')
+    console.log('error')
     }
         })                                                    
     }
@@ -31,7 +36,10 @@ const botlabial = document.querySelector('#labot')
     if (botmaquillaje) {
         
         botmaquillaje.addEventListener('click', function() {
-            unas.classList.add('hidden')
+
+            unas.forEach(item => {
+                item.classList.add('hidden')
+            })
             maquillaje.forEach(item => {
                 item.classList.remove('hidden')
             })
@@ -46,18 +54,43 @@ const botlabial = document.querySelector('#labot')
 }
 
 
-
-botlabial.addEventListener('click', function () {
+ if (botlabial) {
+    botlabial.addEventListener('click', function () {
     labialp.forEach(item => {
         item.classList.remove('hidden')
     })
     maquillaje.forEach(item => {
         item.classList.add('hidden')
-    unas.classList.add('hidden')
-})
+    })
+    unas.forEach(item => {
+    item.classList.add('hidden')
+    })
 })
 
+ } else {
+    console.log('sexo')
+ }
+
+
+ if (botodo) {
+    botodo.addEventListener('click', function () {
+        labialp.forEach(item => {
+            item.classList.remove('hidden')
+        })
+        maquillaje.forEach(item => {
+            item.classList.remove('hidden')
+        })
+        unas.forEach(item => {
+            item.classList.remove('hidden')
+        })
+
+    })
+ } else {
+    console.log('error')
+ }
+
 botfiltro.addEventListener('click', function() { 
+    botodo.classList.toggle('hidden')
     botmaquillaje.classList.toggle('hidden')
     botuna.classList.toggle('hidden')
     botlabial.classList.toggle('hidden')
