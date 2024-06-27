199663071b508c18bc0305076b0fe094ea739f82
Alejo <alejobustamanter@gmail.com>
2024-06-26 21:32:44 -0500

    ahora el boton copiar funciona correctamente

--- copiar.js
+++ copiar.js
@@ -3,7 +3,30 @@ const local = document.querySelector('#local')
 const boton = document.querySelector('#copy')
 const boton2 = document.querySelector('#copy2')
 
+function copiartexto(texto) {
+    navigator.clipboard.writeText(texto).then(() => {
+        console.log('Texto copiado al portapapeles');
+    }).catch(err => {
+        console.error('Error al copiar texto: ', err);
+    });
+}
+
 
 boton.addEventListener('click', function () {
+    copiartexto(numero.textContent)
     boton.textContent = 'copiado'
+    setTimeout( function() {
+        boton.textContent = 'copiar'
+    },3000)
+        
+})
+
+
+boton2.addEventListener('click', function () {
+    copiartexto(local.textContent)
+    boton2.textContent = 'copiado'
+    setTimeout( function() {
+        boton2.textContent = 'copiar'
+    },3000)
+        
 })
