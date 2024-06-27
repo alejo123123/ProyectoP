aa172e0d29d43e32301e51cac747cc13c97320da
Alejo <alejobustamanter@gmail.com>
2024-06-26 19:43:32 -0500

    el js para el boton de copiar

---
+++ copiar.js
@@ -0,0 +1,9 @@
+const numero = document.querySelector('#numero')
+const local = document.querySelector('#local')
+const boton = document.querySelector('#copy')
+const boton2 = document.querySelector('#copy2')
+
+
+boton.addEventListener('click', function () {
+    boton.textContent = 'copiado'
+})
